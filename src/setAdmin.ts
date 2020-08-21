7b45aefa1ab9993d3e635ca411a284df5ee691b4
andykwan1124 <woody.andygrant@gmail.com>
2020-08-20 20:48:30 -0700

    added stricter regex test

--- src/setAdmin.ts
+++ src/setAdmin.ts
@@ -1,7 +1,7 @@
 import { functions, admin } from "./util"
 
 export default functions.auth.user().onCreate(async (user: any) => {
-    if (/@nwplus\.io/.test(user.email)) {
+    if (/.+@nwplus\.io$/.test(user.email)) {
         await admin.auth().setCustomUserClaims(user.uid, { admin: true });
     }
 })
