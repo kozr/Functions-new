2f5d59d6b45da75c692bf30b104fdb2122fdc96e
kozr <zxzflow@gmail.com>
2020-08-31 02:39:59 -0700

    added cors request

--- src/addToMailingList.ts
+++ src/addToMailingList.ts
@@ -1,4 +1,4 @@
-import { functions } from './util';
+import { functions, cors } from './util';
 import { ContactsApi, CreateContact } from 'sib-api-v3-typescript';
 
 const apiInstance: any = new ContactsApi();
@@ -10,37 +10,37 @@ apiKey.apiKey = functions.config().sendinblue.apikey;
 const emailRegExp = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
 
 export default functions.https.onRequest(async (request, response) => {
+  cors(request, response, async () => {
 
-  if (request.method !== 'POST') {
-    response.status(500).send('Invalid request.');
-    return;
-  } else if (request.body.email === '') {
-    response.status(500).send('Please input an email.');
-    return;
-  } else if (!emailRegExp.test(request.body.email)) {
-    response.status(400).send('Email is invalid.');
-    return;
-  }
-
-  try {
-    const contact = new CreateContact();
-    contact.email = request.body.email;
-
-    const res = await apiInstance.createContact(contact);
-    console.log(res);
-    
-    response.status(200).send('Thanks for subscribing!');
-  } catch (error) {
-    const { body: { message } } = error.response;
-    console.error(message);
-
-    switch (message) {
-      case 'Contact already exist':
-        response.status(502).send('You\'ve already subscribed.');
-      default:
-        response.status(502).send(message);
+    if (request.method !== 'POST') {
+      response.status(400).send('Invalid request.');
+      return;
+    } else if (request.body.email === '') {
+      response.status(400).send('Please input an email.');
+      return;
+    } else if (!emailRegExp.test(request.body.email)) {
+      response.status(500).send('Email is invalid.');
+      return;
     }
-    
-  } 
-  
+
+    try {
+      const contact = new CreateContact();
+      contact.email = request.body.email;
+
+      const res = await apiInstance.createContact(contact);
+      console.log(res);
+
+      response.status(200).send('Thanks for subscribing!');
+    } catch (error) {
+      const { body: { message } } = error.response;
+      console.error(message);
+
+      switch (message) {
+        case 'Contact already exist':
+          response.status(409).send('You\'ve already subscribed.');
+        default:
+          response.status(502).send(message);
+      }
+    }
+  })
 })
